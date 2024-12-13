deddc57db6e2b0a158eedbb594326a8d4bf5b806
adeshsinghbits <adeshsingh824@gmail.com>
2024-12-13 21:55:00 +0530

    start and build navbar

---
+++ Appliation-frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
+
+function App() {
+    return (
+        <BrowserRouter>
+                <Header/>
+                {/* Define Routes Here */}
+                <div>adesh</div>
+        </BrowserRouter>
+    );
+}
+
+export default App;
