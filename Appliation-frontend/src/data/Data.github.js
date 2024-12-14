654b67b339ab957ef50af0fe9d0a256772c13399
adeshsinghbits <adeshsingh824@gmail.com>
2024-12-14 15:04:45 +0530

    h

--- Appliation-frontend/src/data/Data.github.js
+++ Appliation-frontend/src/data/Data.github.js
@@ -2,10 +2,19 @@
 export const datafromgithub = async () => {
     const repo_url = "https://api.github.com/repos/adeshsinghbits/javaSript-projects/contents"
     let repoContent = await fetch(repo_url).then((res) => res.json());
-    console.log(repoContent[1].name);
     
-    let data =repoContent.filter((item) => (item.path != "1Application-frontend" ? true : false))
+    let data =repoContent.filter((item) => (item.path != "images" ? true : false))
     data = data.filter((item) => ((item.path).match(/[. /]/g) == null? true : false));
     data = data.map((item) => item.name);
+    return data;
+}
+export const imagefromgithub = async () => {
+    const repo_url = "https://api.github.com/repos/adeshsinghbits/javaSript-projects/contents/images?ref=main"
+    let repoContent = await fetch(repo_url).then((res) => res.json());
+    console.log(repo_url);
+    
+    let data =repoContent.map((item) => item.name);
+    console.log(data);
+    
     return data;
 }
