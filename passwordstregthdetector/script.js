3295148059685d240b16cd69bdadf4bea54f01b9
adeshsinghbits <adeshsingh824@gmail.com>
2024-12-21 21:24:28 +0530

    fixed some issue and create gihub profile project

--- passwordstregthdetector/script.js
+++ passwordstregthdetector/script.js
@@ -1,16 +1,30 @@
 let pass = document.getElementById("pass");
 let bg = document.querySelector(".container");
+let bar = document.querySelector(".bar")
 
 pass.addEventListener("input", (event) => {
     const value = event.target.value
     // console.log(value);
 
     length = value.length
-    blur = 20
+    blur = 13
     blur = blur-length
     // console.log(length);
 
     bg.style.filter= `blur(${blur}px)`
-    console.log(blur);
+    // console.log(blur);
+
+    barLength =  9*length
+    console.log(barLength);
+    
+    bar.style.width = `${barLength}%`
+
+    if (length <= 5) {
+        bar.style.background =  "red"
+    } else if (length <= 10) {
+        bar.style.background =  "yellow"
+    } else {
+        bar.style.background =  "green"
+    }
     
 })
