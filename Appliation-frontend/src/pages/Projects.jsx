98653150811880732e7d87ed8a36767d023137bc
adeshsinghbits <adeshsingh824@gmail.com>
2024-12-21 20:51:57 +0530

    fix issue in project flexing

--- Appliation-frontend/src/pages/Projects.jsx
+++ Appliation-frontend/src/pages/Projects.jsx
@@ -11,7 +11,7 @@ import { datafromgithub } from '../data/Data.github.js';
   const Projects = () => {
     return (
     <div>
-          <div className="flex pb-5 bg-black">
+          <div className="flex pb-5 bg-black flex-wrap justify-between">
               {names.map((name) => (
                       <div key={name}>
                         <Card
